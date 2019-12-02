8574a06001a09ad2ef668afcf53002c69f023f4c
patri <pabels@students.uni-mainz.de>
2019-12-02 10:56:53 +0100

    some blocks added

---
+++ app/src/main/java/com/example/dragandquery/block/BlockFrom.java
@@ -0,0 +1,33 @@
+package com.example.dragandquery.block;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.dragandquery.R;
+import com.example.dragandquery.block.Block;
+
+import java.util.List;
+
+public class BlockSelect implements Block {
+
+    private static final Class<? extends Block>[] sucs = new Class[]{
+            BlockAttribute.class,
+            BlockStar.class};
+
+    @Override
+    public String getName() {
+        return "SELECT";
+    }
+
+    @Override
+    public int getDesign() {
+        return R.drawable.select_block;
+    }
+
+    @Override
+    public Class<? extends Block>[] getSuccessors() {
+        return sucs;
+    }
+}
