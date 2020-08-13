368b1c66f6328bff7b643a51017d37c39948ae1a
PatrickAbels8 <pabels@students.uni-mainz.de>
2020-08-13 08:24:44 +0200

    navdraw done

--- app/src/main/java/com/example/dragandquery/practice/Practices.java
+++ app/src/main/java/com/example/dragandquery/practice/Practices.java
@@ -7,17 +7,31 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewStub;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Space;
 
+import com.example.dragandquery.DbView;
+import com.example.dragandquery.Free;
+import com.example.dragandquery.Impressum;
+import com.example.dragandquery.Navigation;
+import com.example.dragandquery.PopUp;
 import com.example.dragandquery.R;
+import com.example.dragandquery.Settings;
+import com.example.dragandquery.Tutorial;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +42,9 @@ import static com.example.dragandquery.Navigation.SHARED_PREFS;
  * - show ex depending on tut exp
  */
 
-public class Practices extends AppCompatActivity {
+public class Practices extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawer;
 
     //coms
     Context context;
@@ -46,7 +62,23 @@ public class Practices extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_practices);
+        setContentView(R.layout.activity_navigation);
+
+        ViewStub stub = findViewById(R.id.stub);
+        stub.setLayoutResource(R.layout.activity_practices);
+        stub.inflate();
+
+        // nav drawer
+        drawer = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
         //init stuff
         context = getApplicationContext();
@@ -64,12 +96,6 @@ public class Practices extends AppCompatActivity {
         for(int i=0; i<exercises.size(); i++){
             exercises.get(i).setOnClickListener(new MyOnExerciseClickListener());
         }
-
-        //toolbar stuff
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
 
     }
 
@@ -200,6 +226,83 @@ public class Practices extends AppCompatActivity {
         super.onRestart();
         finish();
         startActivity(getIntent());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+            //swipeHint.setVisibility(View.VISIBLE);
+        } else {
+            Intent i = new Intent(getApplicationContext(), PopUp.class);
+            i.putExtra(PopUp.KEY, PopUp.CLOSEAPP);
+            startActivityForResult(i, PopUp.REQUEST_CODE);
+        }
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        //swipeHint.setVisibility(View.GONE);
+        return true;
+    }*/
+
+    @Override
+    //not used yet maybe later for action bar items
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Toast.makeText(getApplicationContext(), "onOptionItemSelected", Toast.LENGTH_SHORT).show();
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Intent i = new Intent(getApplicationContext(), Navigation.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+
+        } else if (id == R.id.nav_free) {
+            Intent i = new Intent(getApplicationContext(), Free.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_tutorial) {
+            Intent i = new Intent(getApplicationContext(), Tutorial.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+
+        } else if (id == R.id.nav_practice) {
+            Intent i = new Intent(getApplicationContext(), Complexity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+
+        } else if (id == R.id.nav_settings) {
+            Intent i = new Intent(getApplicationContext(), Settings.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_impressum) {
+            Intent i = new Intent(getApplicationContext(), Impressum.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_dbView) {
+            Intent i = new Intent(getApplicationContext(), DbView.class);
+            startActivity(i);
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     /***
